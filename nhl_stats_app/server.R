d50d9617e1dfea144f52e6e65af0c55fadaef48f
Taylor Franklin <tfranklin63@gmail.com>
2021-01-16 14:01:36 -0600

    updated Corsi section, created separate tab for Fenwick, updated plots

--- nhl_stats_app/server.R
+++ nhl_stats_app/server.R
@@ -23,59 +23,63 @@ shinyServer(function(input, output) {
     
   })  
   
-  # output$pp_points_plot <- renderPlot({
-  #   
-  #   nhl_stats %>% 
-  #     filter(season == input$trad_season,
-  #            situation != 'all') %>% 
-  #     select(name, points, situation) %>% 
-  #     head(40) %>% #each player has 4 situations so 40 will provide the top 10
-  #     ggplot(aes(x = name, y = points)) + 
-  #     geom_col(aes(fill = situation)) +
-  #     coord_flip()
-    
-  })  
+   output$pp_points_plot <- renderPlot({
   
-  output$possession_indiv <- renderPlotly({
-    
-    corsi_goals_indiv <- nhl_stats %>% 
-      filter(season == input$season,
-             situation == input$situation,
-             icetime > 8000) %>% 
-      select(name, position, onIce_corsiPercentage, goals) %>% 
-      ggplot(aes(x = onIce_corsiPercentage, y = goals, text = name)) +
-      geom_point(aes(color = position))
-    
-    ggplotly(corsi_goals_indiv, tooltip = c('text', 'x', 'y'))
+     top_points <- nhl_stats %>% 
+       filter(season == input$trad_season) %>% 
+       arrange(desc(points)) %>% 
+       head(10) %>% 
+       pull(name)
+     
+     nhl_stats %>%
+       filter(season == input$trad_season,
+              situation != 'all',
+              name %in% top_points) %>%
+       select(name, points, situation) %>%
+       ggplot(aes(x = name, y = points)) +
+       geom_col(aes(fill = situation)) +
+       coord_flip()
+
   })
   
-  output$possession_plot <- renderPlotly({
+  output$corsi_team_plot <- renderPlotly({
     
-    corsi_goals_by_team <- nhl_stats %>% 
-      filter(season == input$season, 
-             situation == input$situation) %>% 
-      group_by(team) %>% 
+    corsi_goals_by_team <- nhl_stats %>%
+      filter(situation == '5on5') %>% 
+      group_by(team, season) %>% 
       summarise(team_corsi_average = mean(onIce_corsiPercentage),
                 team_goals = sum(goals)) %>% 
       ungroup() %>% 
-      select(team, team_corsi_average, team_goals) %>% 
-    ggplot(aes(x = team_corsi_average, y = team_goals, text = team)) + 
+      select(team, season, team_corsi_average, team_goals) %>% 
+    ggplot(aes(x = team_corsi_average, y = team_goals, label = team, text = season)) + 
     geom_point()
   
-  ggplotly(corsi_goals_by_team, tooltip = c('text', 'x', 'y'))
+  ggplotly(corsi_goals_by_team, tooltip = c('label', 'text', 'x', 'y'))
       
     
   })  
   
-  output$possessionTable <- renderDT({
+  output$corsiTable <- renderDT({
       
       nhl_stats %>% 
-        filter(season == input$season, 
-               situation == input$situation) %>% 
-        select(player_info, possession) %>% 
+        filter(season == input$corsi_season, 
+               situation == '5on5',
+               icetime >= input$corsi_icetime) %>% 
+        select(player_info, possession, -onIce_fenwickPercentage, -offIce_fenwickPercentage) %>% 
         arrange(desc(onIce_corsiPercentage))
       
     })
+  
+  output$fenwickTable <- renderDT({
+    
+    nhl_stats %>% 
+      filter(season == input$fenwick_season, 
+             situation == '5on5',
+             icetime >= input$fenwick_icetime) %>% 
+      select(player_info, possession, -onIce_corsiPercentage, -offIce_corsiPercentage) %>% 
+      arrange(desc(onIce_fenwickPercentage))
+    
+  })
     
   output$goals_plot <- renderPlotly({
     
@@ -89,6 +93,8 @@ shinyServer(function(input, output) {
       select(team, team_xG, team_goals) %>% 
       ggplot(aes(x = team_xG, y = team_goals, text = team)) + 
       geom_point()
+    
+    #look for y = x line
     
     ggplotly(xG_goals_by_team, tooltip = c('text', 'x', 'y'))
   
